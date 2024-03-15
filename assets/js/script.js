834779832f426f9fdb1d8ef7ef15a19032a9a6b6
rinklepoonia <rinklepoonia889@gmail.com>
2024-03-15 14:05:08 +0530

    breaking-news section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,50 @@
 const menulist = document.querySelector(".menulist");
 const menuIcon = document.querySelector(".menuIcon");
-// const s = document.querySelector(".s")
 
 menuIcon.addEventListener("click", function () {
     menulist.classList.toggle("show");
     menuIcon.classList.toggle("show");
     document.querySelector(".menuIcon").classList.toggle("cross")
 })
-// menuIcon.addEventListener("click", function () {
-//     menulist.classList.toggle("show");
-//     document.body.classList.toggle("overFlowHidden")
-//     document.querySelector(".menuIcon").classList.toggle("cross")
-// })
+
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+    // cssEasy: 'linear',
+    speed: 300,
+    arrow: true,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots: false,
+                infinite: true,
+
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+            }
+        },
+    ]
+});
