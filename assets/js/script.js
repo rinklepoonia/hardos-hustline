392b56668b5f384c7f34272a7829c4aca6b47d3d
rinklepoonia <rinklepoonia889@gmail.com>
2024-03-18 11:28:09 +0530

    back to top and preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,33 @@ menuIcon.addEventListener("click", function () {
     document.querySelector(".menuIcon").classList.toggle("cross")
 })
 
+// preloder
+const Preload = document.getElementById("preload")
+setTimeout(() => {
+    Preload.classList.add("d-none")
+}, "4000");
+
+// back-to-top
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
+
 $('.responsive').slick({
     dots: false,
     infinite: true,
-    // autoplay: true,
-    // autoplaySpeed: 1000,
-    // cssEasy: 'linear',
+    autoplay: true,
+    autoplaySpeed: 1000,
+    cssEasy: 'linear',
     speed: 300,
     arrow: true,
     prevArrow: ".prev",
@@ -31,7 +52,7 @@ $('.responsive').slick({
             }
         },
         {
-            breakpoint: 768,
+            breakpoint: 992,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 1,
