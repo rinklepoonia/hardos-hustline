9e30facb2fe8448964aed75fc20950bd9f5ee0c6
rinklepoonia <rinklepoonia889@gmail.com>
2024-03-19 10:29:50 +0530

    nav-slider, solve error ,

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,47 @@ minusBtn.addEventListener("click", function () {
     count.innerHTML = count.innerHTML - 1;
 })
 
+$('.nav-slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    cssEasy: 'linear',
+    speed: 300,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                dots: false,
+                infinite: true,
+                autoplay: true,
+
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: false,
+            }
+        },
+    ]
+});
+
+
 $('.responsive').slick({
     dots: false,
     infinite: true,
