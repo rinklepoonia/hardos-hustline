016a57251c7bbfd8ce4da75da42c3e1d537c0991
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-05 16:24:47 +0530

    left-right border in timeline section

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ const topBtn = document.querySelector(".topBtn");
 const show = document.querySelector(".show");
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 60) {
+    if (window.scrollY > 560) {
         topBtn.classList.add("show");
     } else {
         topBtn.classList.remove("show");
@@ -108,7 +108,6 @@ $('.responsive').slick({
             breakpoint: 992,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1,
                 dots: true,
             }
         },
@@ -160,3 +159,47 @@ $('.team-slider').slick({
         },
     ]
 });
+
+
+
+// Calling showTime function at every second
+setInterval(showTime, 1000);
+
+// Defining showTime funcion
+function showTime() {
+    // Getting current time and date
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+
+    // Setting time for 12 Hrs format
+    if (hour >= 12) {
+        if (hour > 12) hour -= 12;
+        am_pm = "PM";
+    } else if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+
+    hour =
+        hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    let currentTime =
+        hour +
+        ":" +
+        min +
+        ":" +
+        sec +
+        am_pm;
+
+    // Displaying the time
+    document.getElementById(
+        "clock"
+    ).innerHTML = currentTime;
+}
+
+showTime();
